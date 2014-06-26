c345a72f5982385757509e86bf5e31de76e9c89d
Zbigniew 'zibi' Jarosik <zbigniew.jarosik@ifresearch.org>
2014-06-26 11:55:01 +0200

    numeric keys as arrays

--- src/IFR/Cli/Config.php
+++ src/IFR/Cli/Config.php
@@ -7,22 +7,23 @@ class Config
 	public static $config = null;
 	private static $file = null;
 
-	public function __construct($array=array())
+	public function __construct($array = array())
 	{
 		if(count($array))
 		{
 			self::$config = self::object($array);
 		}
 	}
-	public static function get($path=null)
+
+	public static function get($path = null)
 	{
 		$config = self::$config;
 
 		if(!is_null($path))
 		{
-			$path = trim($path,'/');
+			$path = trim($path, '/');
 
-			$chunks = explode('/',$path);
+			$chunks = explode('/', $path);
 
 			foreach($chunks as $chunk)
 			{
@@ -39,47 +40,75 @@ class Config
 
 		return $config;
 	}
-	public static function load($file = null,$stage = 'production')
+
+	public static function load($file = null, $stage = 'production')
 	{
 		if(is_null($file))
 		{
-			$file = dirname($_SERVER['PHP_SELF']).'/config/config.ini';
+			$file = dirname($_SERVER['PHP_SELF']) . '/config/config.ini';
 		}
 
 		self::$file = $file;
 
-		    $config = parse_ini_file(self::$file, true);
+		$config = parse_ini_file(self::$file, true);
 
-			if(is_file(self::$file.'.local')) 
-		    {
-			$config_local = parse_ini_file(self::$file.'.local', true);
+		if(is_file(self::$file . '.local'))
+		{
+			$config_local = parse_ini_file(self::$file . '.local', true);
 			$config = array_replace_recursive($config, $config_local);
-		    }
-		
+		}
+
 		self::$config = self::object($config[$stage]);
 	}
+
 	static function object($array = array())
 	{
-		$obj = new Config(null);
+		$numeric = self::is_allKeysNumeric($array);
 
-		foreach($array as $key=>$value)
+		$obj = $numeric?array():new Config(null);
+
+		foreach($array as $key => $value)
 		{
 			if(is_array($value))
 			{
 				$value = self::object($value);
 			}
-			$obj->$key = $value;
+
+			if($numeric)
+			{
+				$obj[$key] = $value;
+			}
+			else
+			{
+				$obj->$key = $value;
+			}
 		}
 
 		return $obj;
 	}
+
 	public function __get($name)
 	{
-		return isset(self::$config->{$name})?self::$config->{$name}:null;
+		return isset(self::$config->{$name}) ? self::$config->{$name} : null;
 	}
 
-    public function configGet($path)
-    {
-        return self::get($path);
-    }
+	public function configGet($path)
+	{
+		return self::get($path);
+	}
+
+	static function is_allKeysNumeric($array)
+	{
+		$ret = true;
+
+		foreach($array as $key=>$value)
+		{
+			if(!is_numeric($key))
+			{
+				$ret = false;
+			}
+		}
+
+		return $ret;
+	}
 }
